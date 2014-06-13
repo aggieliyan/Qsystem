3627f771b21b9e5eceb0a25985db2eb5b3de9de5
aggieliyan <yanli@ablesky.com>
2014-06-13 20:05:38 +0800

    2121

--- project/urls.py
+++ project/urls.py
@@ -3,4 +3,6 @@ from project import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.new_project, name='new_project'),
+	#url(r'^create/', views.create, name='create'),
+	#url(r'^showperson', views.show_person),
 )
