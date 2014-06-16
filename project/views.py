fb4be922a3aad42bfa8993d917f74c5d02420bd8
wli <wli@ablesky.com>
2014-06-16 10:37:19 +0800

    add homepage forms

--- project/views.py
+++ project/views.py
@@ -5,7 +5,6 @@ from django.template import Context
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import RequestContext
 import forms
-
 import json
 from django.db import connection
 import MySQLdb
@@ -13,6 +12,7 @@ from django.contrib.sessions.models import Session
 import datetime
 from django.core.urlresolvers import reverse
 from django.db.models import Q
+from forms import changedesignForm,delayprojectForm
 from project.models import *
 from models import public_message
 from models import project_user 
