c1894beaf1730f222add9bc5c622ed1133c75696
gaoyue001 <775694463@qq.com>
2014-06-18 10:51:30 +0800

    Update views.py

--- project/views.py
+++ project/views.py
@@ -302,8 +302,7 @@ def personal_homepage(request):
     
     #userid = request.session['id']
     userid='1'
-    messagess=public_message.objects.raw('select a.id,a.content,a.isactived,a.project_id,a.publication_date,a.publisher_id,a.type_p from project_public_message as a,project_project_user as  b WHERE  a.project_id=b.project_id and a.isactived=1 and b.username_id=%s ORDER BY a.id desc',[userid])
-    i=0
+messagess=public_message.objects.raw('select a.id,a.content,a.isactived,a.project_id,a.publication_date,a.publisher_id,a.type_p from project_public_message as a,project_project_user as  b WHERE  a.project_id=b.project_id and a.isactived=1 and a.type_p=\'message\' and b.username_id=%s ORDER BY a.id desc',[userid])    i=0
     for item in messagess:
       i=i+1 
     count=i
