3fbf3e450401fa30e063d979f72ddc20e84ac51e
yilu282 <1049924595@qq.com>
2014-06-30 16:38:28 +0800

    打印一些东西调试

--- project/views.py
+++ project/views.py
@@ -588,12 +588,15 @@ def Insert_user(request,id,id2):
         user=models.user.objects.filter(department_id=department_id,realname=realname).update(Position_level='3')
     elif id=='4':
         realname=request.POST['level_1']
+        print type(realname)
         user=models.user.objects.filter(department_id=department_id,realname=realname).update(Position_level='0')
     elif id=='5':
         realname=request.POST[id2]
+        print type(realname)
         user=models.user.objects.filter(department_id=department_id,realname=realname).update(Position_level='0')
     elif id=='6':
         realname=request.POST[id2]
+        print type(realname)
         user=models.user.objects.filter(department_id=department_id,realname=realname).update(Position_level='0')
     elif id=='7':
         realname=request.POST['level_1']
