d563b1454df6fc9f54297f90eff20cb87d329a77
gaoyue001 <775694463@qq.com>
2014-07-03 15:44:54 +0800

    格式

--- project/views.py
+++ project/views.py
@@ -263,7 +263,7 @@ def new_project(request, pid=''):
     
 
 def project_list(request):
-	
+	#notice
     noticess=public_message.objects.filter(type_p='notice').order_by('-id')
     count=len(noticess)
     notices=noticess[:5]
@@ -280,8 +280,7 @@ def project_list(request):
         a = 0
     else:
         a = len(noticess)-5    	
-	
-
+    ##
     projectlist = None
     puser=None
     project_name=""
@@ -470,13 +469,11 @@ def personal_homepage(request):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         projectobj = paginator.page(paginator.num_pages)
-    
-    #userid = request.session['id']
+    #message
     userid=request.session['id']
     j=0
     if request.user.has_perm('project.change_project_delay'):
         j=1
-    
     i=0
     tests= project_user_message.objects.filter(userid_id=userid)
     lists=[]
@@ -484,12 +481,11 @@ def personal_homepage(request):
     for test in tests:
         lists.append(test.messageid_id)
         messagess = public_message.objects.filter(pk__in=lists).filter(type_p = "message").order_by('-publication_date')
-  
     for item in messagess:
         i=i+1 
     count=i
     messages=messagess[:4]
-   
+    ##
     return render_to_response('personal_homepage.html',
         {'projectobj':projectobj,'result':result,'result1':result1,'puser':puser,'messages': messages,'count':count,'j':j})
 
