bf50c81f8d89ba45395fbe7f8d359eb38236f965
gaoyue001 <775694463@qq.com>
2014-07-09 18:33:19 +0800

    初始化数据中文

--- project/views.py
+++ project/views.py
@@ -980,7 +980,7 @@ def initdata(request):
     depart3.save()
     depart4 = department(id=4,department='客户端开发',isactived=1)
     depart4.save()
-    return HttpResponse("Data has been inited successfully~")
+    return HttpResponse("恭喜你,初始化数据成功~")
 
   
     
