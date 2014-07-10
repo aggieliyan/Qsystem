064300c1ca33bb47067c78a480a749d007f9e4d3
yilu282 <1049924595@qq.com>
2014-07-10 18:05:26 +0800

    Revert "通用头修改"
    This reverts commit ad890e1ce8b420b346b2423d374343301bada119.

--- project/static/js/base.js
+++ project/static/js/base.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
 
-	//var url = "/showuser";
-	//$.get(url, function(data){
-	//	var user = eval("\("+data+"\)");
-	//	var name = user.realname;
-	//    $(".top_bar a").eq(0).text(name);
-	//    if(name == "GUEST"){
-	 //   	var bar = $(".top_bar a").eq(1)
-	//    	bar.text("登录");
-	//    	bar.attr("href","/login");
+	var url = "/showuser";
+	$.get(url, function(data){
+		var user = eval("\("+data+"\)");
+		var name = user.realname;
+	    $(".top_bar a").eq(0).text(name);
+	    if(name == "GUEST"){
+	    	var bar = $(".top_bar a").eq(1)
+	    	bar.text("登录");
+	    	bar.attr("href","/login");
 
-	//    }
-	//});
+	    }
+	});
 
 	$(".judge").click(function()
 	{
