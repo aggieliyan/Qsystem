e2b69ad34cc316e13a4ba1d81107c7ad37957e30
yilu282 <1049924595@qq.com>
2015-01-28 18:25:12 +0800

    fix点某个小三角,所有的下拉内容都出来的BUG

--- project/static/js/slist.js
+++ project/static/js/slist.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
 	$(".flip").click(function(){
-		$(".flip").toggleClass("drapdown");
-		$(".panel").toggle();
+		$(this).toggleClass("drapdown");
 		var panel = $(this).parent().parent().next(".panel");
 		console.log(panel.length);
         content="这是一个内容很好<br/>这是一个内容很好<br/>这是一个内容很好<br/>这是一个内容很好<br/>"
 		if(panel.length == 0){
 			$(this).parent().parent().after("<tr class='row panel'><td><span>"+content+"<span></td></tr>");
+		}else{
+			panel.toggle();
 		}
 
 	});
