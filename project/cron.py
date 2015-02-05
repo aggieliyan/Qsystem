885d9e2191310dd9424b39d5369a7b95209cef7e
aggieliyan <yanli@ablesky.com>
2015-02-05 17:12:38 +0800

    非整数不生成趋势图,并将它的绘图标致改为不生成图

--- project/cron.py
+++ project/cron.py
@@ -15,8 +15,14 @@ def my_scheduled_job(request):
     chart_pro = models.project_statistics.objects.filter(is_graph=1)
     results = []
     for item in chart_pro:
-        results.append(models.project_statistics_result(project_id=item.project_id, sql_id=item.id,
+        try:
+            int(item.total)
+            results.append(models.project_statistics_result(project_id=item.project_id, sql_id=item.id,
                                                         date=datetime.datetime.now(), 
                                                         statistical_result=item.total, isactived=1))
+        except:
+            ps = models.project_statistics.objects.get(id=item.id)
+            ps.is_graph = 0
+            ps.save()
     models.project_statistics_result.objects.bulk_create(results)
     return HttpResponse("123")
