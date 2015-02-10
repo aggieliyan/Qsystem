e048075d7f88e1882bde7a35a2cfdff65f2effaa
yilu282 <1049924595@qq.com>
2015-02-10 14:17:57 +0800

    压缩

--- project/static/js/newproject.js
+++ project/static/js/newproject.js
@@ -28,11 +28,10 @@ $(document).ready(function(){
       }
       var p = $("[title='1']").children();
       for(var i=0;i<p.length;i++){
-        //选中之前已经选过的-改成之前选过的不显示
-        //$("#"+p.eq(i).attr("value")).attr("checked","checked");
+/*        选中之前已经选过的-改成之前选过的不显示*/
         $("#"+p.eq(i).attr("value")).parent().attr("style","display:none;");
       } 
-      //全选恢复为未选择状态
+/*      全选恢复为未选择状态*/
       $(".all input").removeAttr("checked");
     }
 
@@ -43,7 +42,7 @@ $(document).ready(function(){
           return item.attr("value");
         }
       }
-      //没找到
+/*      没找到*/
       return ''
     }
     
@@ -96,7 +95,7 @@ $(document).ready(function(){
     });
 
 
-    //选择人员后点确定，
+/*    选择人员后点确定，*/
     $("#selectok").click(function(){
       var testerlist = $("#testerlist input");
     	var jsontester = { tester:[]};
@@ -110,11 +109,11 @@ $(document).ready(function(){
 		      jsontester.tester.push(temp);
 	      }
 	    }
-      //$("[title='1']").children("div").remove();
-      //在页面显示已选人员
+
+/*      在页面显示已选人员*/
       var radioname = Math.floor(Math.random()*9999+1).toString();
       var exsituser = $("[title='1']").children("div");
-      //把已在的用户name都更新一下
+/*      把已在的用户name都更新一下*/
       for(var item=0;item<exsituser.length;item++){
         exsituser.children('input').eq(item).attr("name", radioname)
       }
@@ -124,10 +123,10 @@ $(document).ready(function(){
         $("[title='1']").append('<div class="role-item" value="' + jsontester.tester[i].value+'"><input type="radio" name='+radioname+'><span>'+jsontester.tester[i].name+'</span><span class="close">x</span></div>');
       }
      
-      //跟数量调整高度
+/*      跟数量调整高度*/
       adjust_height();
 
-      //负责人还在的话要选中
+/*      负责人还在的话要选中*/
 /*      var dv = $("[name='designer']").attr("value")
       var tv = $("[name='tester']").attr("value")
       if(dv){
@@ -137,21 +136,22 @@ $(document).ready(function(){
         $("[value='"+tv+"'] input").attr("checked","checked");
       }*/
 
-      //设置项目参与人员
+/*      设置项目参与人员*/
       count_relateduser()
 
-      //设置产品测试负责人
+/*      设置产品测试负责人*/
       set_other_master()
 
     });
 
 
-    //删除人员,重新数一遍参与人员
+/*    删除人员,重新数一遍参与人员*/
     $(".role-item .close").live('click',function(){
         var lv = $("[name='leader']").attr("value");
         if(lv){
           var rv = $(this).parent().attr("value");
-          if(rv == lv){//要删的是负责人的话提示不能删
+          if(rv == lv){
+
             alert("不能移除项目负责人");
           }else{
             $(this).parent().remove();
@@ -164,7 +164,7 @@ $(document).ready(function(){
         set_other_master()
     });
     
-    //点添加，弹出人员选择框展示数据
+/*    点添加，弹出人员选择框展示数据*/
     $(".roles .btn-success").click(function(){    
       $("#select").modal("show");
       $("#skey").attr("value","");
@@ -195,7 +195,6 @@ $(document).ready(function(){
         anum < pnum ? pagenum = anum+1 : pagenum = anum
         $(".pagination ul li").remove()
         $(".pagination ul").append("<li><a>&laquo;</a></li><li><a>&raquo;</a></li>");
-        //$(".pagination ul").append("<li><a href=\"#\"></a></li><li><a href=\"#\"></a></li>");
         for(var i=0; i<pagenum; i++){
           var page = i+1;
           $(".pagination ul li:last").before("<li><a>"+page+"</a></li>");
@@ -212,21 +211,20 @@ $(document).ready(function(){
 
     });
 
-   //点页码
+/*   点页码*/
   $(".pagination li").live('click',function(){
     var page = parseInt($(this).text());
-    //console.log(allperson);
     var start = (page-1)*pagemaxnum;
     person = allperson.slice(start, start+pagemaxnum);
     show_staff(person);
 
   });
-  //点全选
+/*  点全选*/
   $(".all input").click(function(){
     var a = $(".table-list input");
     if($(this).attr("checked")=="checked"){ 
       for(var i=0;i<a.length;i++){
-        //如果是隐藏的-及已在人员列表中则不选中
+/*        如果是隐藏的-及已在人员列表中则不选中*/
         if(a.eq(i).parent().attr("style")=="display:none;"){
           continue;
         }
@@ -240,14 +238,14 @@ $(document).ready(function(){
 
   });
 
-   //选择人员框里搜索
+/*   选择人员框里搜索*/
   $("#psearch").click(function(){
       var skey = $.trim($("#skey").val());//
       var role = $("[title='1']").prev("button").attr("name");
       
       url = "/psearch"
       para = {"key":skey, "role":role}
-        //"key="+skey
+/*        "key="+skey*/
       $.get(url, para, function(data){
           var person = eval('('+data+')');
           allperson = person.person;
@@ -275,7 +273,7 @@ $(document).ready(function(){
 
    });
 
-    //选择项目负责人
+/*    选择项目负责人*/
     $("#master").focus(function(){
       p = $(".role-item");  
       //没有值的时候提示去添加人员
