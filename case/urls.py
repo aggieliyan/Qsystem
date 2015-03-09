87290a7d9db013a8ad2ebd28ef410c47ba2034df
aggieliyan <yanli@ablesky.com>
2015-03-09 17:01:02 +0800

    文件

--- case/urls.py
+++ case/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
-from case import cases_views, login_views, category_views
+from case import cases_views, login_views, category_views, casepull_views
 
 urlpatterns = patterns('',
 	url(r'^login/', login_views.login, name='login'),
 	url(r'^caselist/', cases_views.case_list, name='caselist'),
-	url(r'^procate/', category_views.product_category, name='procate')
-
+	url(r'^procate/', category_views.product_category, name='procate'),
+	url(r'^pull/', casepull_views.case_pull, name='casepull'),
 )
