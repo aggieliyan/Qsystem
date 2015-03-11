e6fff880b4ea3ffc0362b9cb98127bd6b89ec80a
aggieliyan <yanli@ablesky.com>
2015-03-11 14:26:00 +0800

    ok, 把我电脑name做了个判断

--- Qsystem/settings.py
+++ Qsystem/settings.py
@@ -219,12 +219,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    'project/static/',
-    'common/static/',
-    'case/static/',
-    )
 
+if socket.gethostname() == 'liyan-PC':
+    STATICFILES_DIRS = (
+        'Qsystem/common/static/',
+        )
+else:
+    STATICFILES_DIRS = (
+        'common/static/',
+        )
 TEMPLATE_LOADERS = (
    'django.template.loaders.app_directories.Loader',
 )
