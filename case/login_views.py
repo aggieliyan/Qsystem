0774547d739965f59368aab9062f68547615a43f
wli <wli@ablesky.com>
2015-04-24 16:17:49 +0800

    退出清除cookie

--- case/login_views.py
+++ case/login_views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render_to_response, redirect, HttpResponseRedirect
+from django.contrib.sessions.models import Session
 
 def logout(request):
 	try:
 		response = HttpResponseRedirect("/case/login")
 		response.delete_cookie("username")
 		response.delete_cookie("password")
+
 		session_key = request.session.session_key
 		Session.objects.get(session_key=session_key).delete()
 		return response
 	except:
 		pass
 	return HttpResponseRedirect("/case/login/")
-
-def no_login(request):
-    return render_to_response("nologin.html")
-
-def no_perm(request):
-    return render_to_response("noperm.html")
