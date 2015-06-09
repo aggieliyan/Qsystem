1cd46ef605c64fbf8e6dc248de036c3319ca16ad
aggieliyan <yanli@ablesky.com>
2015-06-09 15:24:11 +0800

    去了

--- Qsystem/settings.py
+++ Qsystem/settings.py
@@ -92,9 +92,7 @@ PIPELINE_CSS = {
           'css/login.css',
         ),
         'output_filename': 'css/colors.css',
-        'PIPELINE_CSS_COMPRESSOR': {
-            'None',
-        }
+      
     }
 }
 
