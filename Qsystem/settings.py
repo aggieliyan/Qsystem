92cdb2c70255835507801cfed317dba965359f5f
aggieliyan <yanli@ablesky.com>
2015-06-10 11:26:51 +0800

    文档坑啊。。试试改个

--- Qsystem/settings.py
+++ Qsystem/settings.py
@@ -87,7 +87,9 @@ else:
 
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 PIPELINE_YUGLIFY_BINARY = '/usr/local/bin/yuglify'
-
+PIPELINE_CSS_COMPRESSOR = (
+        'pipeline.compressors.yuglify.YuglifyCompressor',
+        )
 PIPELINE_CSS = {
     'colors': {
         'source_filenames': (
