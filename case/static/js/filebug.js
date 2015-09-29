df4af8acef51654fb0c7ec2ecf77e758494262ac
aggieliyan <yanli@ablesky.com>
2015-09-29 17:05:01 +0800

    冲突没处理完

--- case/static/js/filebug.js
+++ case/static/js/filebug.js
@@ -145,12 +145,8 @@ $(document).ready(function(){
         }); 
 		$('#fileBugModal').modal('hide'); 		
      });
-<<<<<<< HEAD
-    $('#file_upload1').uploadify({
-		'formData': {'request.session["realname"]':''},		
-=======
+
     $('#file_upload1').uploadify({		
->>>>>>> origin/master
         'swf'  : '/static/jquery/uploadify.swf',
         'uploader'  : '/case/upload_script/',  
         'auto'      : true , 
