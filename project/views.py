30da247917da32d20c66e13b0e1072f3a77792f9
aggieliyan <yanli@ablesky.com>
2015-10-13 15:10:18 +0800

    看看咋回事

--- project/views.py
+++ project/views.py
@@ -887,6 +887,7 @@ def project_list(request):
     # p1 = models.project_statistics.objects.distinct().values_list('project_id',flat=True)
     # pcount = list(set(p1).intersection(set(proid))) 
     testsql = []
+    m = 0
     for c in pcount: 
         sql = c.sql
         db = c.db
@@ -906,13 +907,14 @@ def project_list(request):
             c.save()
             cursor.close()
         except:
+            m = m + 1
             pass
         if c.project_id not in cpcount: #project_id 去重
             filter_project.append(pcount.filter(project_id=c.project_id).order_by("total")[0]) #每个项目只返回一组统计值最大的记录,方便页面显示
             cpcount.append(c.project_id)   
     print datetime.datetime.now()
     if 'testsql' in request.path:
-        return HttpResponse(testsql)
+        return HttpResponse(m)
     return render_to_response('projectlist.html', RequestContext(request, {'projectobj':projectobj, \
             'rendering':rendering, 'pcount':pcount, 'fproject':filter_project,  'project_id':project_id, \
             'project_name':project_name, 'start_date_s':start_date_s, 'end_date_s':end_date_s, \
