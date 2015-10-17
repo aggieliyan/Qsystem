8b385ea1ecf37336d3db52bdaddabd724ea795fb
aggieliyan <yanli@ablesky.com>
2015-10-17 10:23:44 +0800

    改成db缓存试试

--- Qsystem/settings.py
+++ Qsystem/settings.py
@@ -256,16 +256,16 @@ CRONJOBS = [
     ]
 
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
-CACHE_MIDDLEWARE_SECONDS = 1
-
+#CACHES = {
+#    "default": {
+#        "BACKEND": "django_redis.cache.RedisCache",
+#        "LOCATION": "redis://127.0.0.1:6379/1",
+#        "OPTIONS": {
+#            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#        }
+#    }
+#}
+#SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+#SESSION_CACHE_ALIAS = "default"
+#CACHE_MIDDLEWARE_SECONDS = 100
+CACHE_BACKEND = 'db://q_cache_table'
